a4e673914469ea599b9c86a74ec56051bafe7fd7
abedezz <aezideen@smail.ucas.edu.ps>
2021-02-26 16:36:20 +0200

    طباعة الرقم 10

--- src/adeb/ezz/Main.java
+++ src/adeb/ezz/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println(5000+400);
+        System.out.println(10);
+//        fknvkjdnfkvndfkg
+//        youssef is fishing
     }
 }
